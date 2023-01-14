5f8454087c27085049d979735882ba8823ecc8fc
prestonButton <prestonbutton99@gmail.com>
2023-01-14 15:37:44 -0500

    Trying new slideshow model

--- scripts.js
+++ scripts.js
@@ -1,25 +1,15 @@
-// SlideShow Code
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 5 seconds
-}
-
-// navbar code
+/* Navbar Code:
+This code will hide the navbar when the user scrolls down 
+and show it when the user scrolls up.
+*/
 
 window.addEventListener("scroll", () => {
   const navbar = document.querySelector(".nav");
 
   navbar.classList[window.scrollY > 50 ? "add" : "remove"]("hide");
 });
+
+/* Slideshow Code:
+This code will display the images in a slideshow. 
+*/
+
