4b2a1fc99bed77a5740bf457a052a80faab6b0f6
Restyled.io <commits@restyled.io>
2023-01-14 22:10:52 +0000

    Restyled by prettier

--- email.js
+++ email.js
@@ -1,6 +1,6 @@
 const btn = document.getElementById("button");
 
-document.getElementById("form").addEventListener("submit", function(event) {
+document.getElementById("form").addEventListener("submit", function (event) {
   event.preventDefault();
 
   btn.value = "Sending...";
@@ -8,16 +8,16 @@ document.getElementById("form").addEventListener("submit", function(event) {
   const serviceID = "default_service";
   const templateID = "cmw-contact-form";
 
-  emailjs.sendForm(serviceID, templateID, this)
-      .then(
-          () => {
-            btn.value = "Send Email";
-            alert("Sent!");
-          },
-          (err) => {
-            btn.value = "Send Email";
-            alert(JSON.stringify(err));
-          });
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      btn.value = "Send Email";
+      alert("Sent!");
+    },
+    (err) => {
+      btn.value = "Send Email";
+      alert(JSON.stringify(err));
+    }
+  );
   // reset form
   document.querySelector("form").reset();
 });
