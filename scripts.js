869e08689004548b2c59d95f518ca5b20d44fee6
Preston Button <98365800+prestonButton@users.noreply.github.com>
2023-01-24 13:46:04 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -34,5 +34,5 @@ function showSlides() {
     slideIndex = 1;
   }
   slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 5 seconds
+  setTimeout(showSlides, 10000); // Change image every 10 seconds
 }
