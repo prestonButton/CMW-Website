a9de7e55237bda46629926976915654f510b1949
prestonButton <prestonbutton99@gmail.com>
2023-01-25 21:23:28 -0500

    Got to where I want it to go live as template

--- scripts.js
+++ scripts.js
@@ -27,7 +27,7 @@ function showSlides() {
     slideIndex = 1;
   }
   slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 5 seconds
+  setTimeout(showSlides, 10000); // Change image every 5 seconds
 }
 
 /*add responsiveness to the navbar*/
