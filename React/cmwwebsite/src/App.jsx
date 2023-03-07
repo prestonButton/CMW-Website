89911cba33e06d6505df39dd960b8ef9c14f9340
Preston Button <prestonbutton99@gmail.com>
2023-03-07 13:33:13 -0500

    add image folder

--- React/cmwwebsite/src/App.jsx
+++ React/cmwwebsite/src/App.jsx
@@ -1,29 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Gallery from './components/Gallery';
-import Contact from './components/Contact';
-import './App.css';
-import './components/Nav.css';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Gallery from "./components/Gallery";
+import Contact from "./components/Contact";
+import "./App.css";
+import "./components/Nav.css";
+import Logo from "./assets/logo.svg";
 
 export default function App() {
   return (
     <Router>
-      <nav className='navbar'>
+      <nav className="navbar">
         <Link to="/" className="navLink">
-          Home
+          <img src={Logo} alt="Logo" />
         </Link>
-        <Link to="about" className="navLink">
-          About
-        </Link>
-        <Link to="gallery" className="navLink">
-          Gallery
-        </Link>
-        <Link to="contact" className="navLink">
-          Contact
-        </Link>
+        <div className="linksDiv">
+          <Link to="/" className="navLink">
+            Home
+          </Link>
+          <Link to="about" className="navLink">
+            About
+          </Link>
+          <Link to="gallery" className="navLink">
+            Gallery
+          </Link>
+          <Link to="contact" className="navLink">
+            Contact
+          </Link>
+        </div>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
---
+++ React/cmwwebsite/src/assets/assets.md
@@ -0,0 +1,10 @@
+# Assets
+
+This file will cover the naming of images for the slideshow section
+
+1. I suggest naming of images should follow the standard set previously: image(number).png
+2. start indexing images at 0 is recommended 
+3. .png file format is suggested
+4. Less than 10 images is preferred
+5. crop images to the standard 3:2 landscape position. This is highly recommended as otherwise it will create jumpiness on the page.
+6. if deleting an image, make sure to not delete the favicon.ico or the logo.png from the assets folder.
