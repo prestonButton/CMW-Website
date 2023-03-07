d71710d54f49710eefac5abb4e2a1f25bdeffd7e
Preston Button <prestonbutton99@gmail.com>
2023-03-07 11:37:47 -0500

    set up router and routes

--- React/cmwwebsite/src/App.jsx
+++ React/cmwwebsite/src/App.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Gallery from './components/Gallery';
+import Contact from './components/Contact';
+
 
 export default function App() {
   return (
     <Router>
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="about">About</Link>
-        <Link to="gallery">Gallery</Link>
-        <Link to="contact">Contact</Link>
+        <Link to="/" className="navLink">
+          Home
+        </Link>
+        <Link to="about" className="navLink">
+          About
+        </Link>
+        <Link to="gallery" className="navLink">
+          Gallery
+        </Link>
+        <Link to="contact" className="navLink">
+          Contact
+        </Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="gallery" element={<Gallery />} />
+        <Route path="contact" element={<Contact />} />
       </Routes>
     </Router>
   );
