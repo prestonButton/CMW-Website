789c4d03cb49c5858ab0379af2c411d50f83c2bf
Preston Button <prestonbutton99@gmail.com>
2023-03-09 14:23:15 -0500

    set background image of home with opacity

--- React/cmwwebsite/src/pages/Home.jsx
+++ React/cmwwebsite/src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import "./Home.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="Home--header_div">
+      <div className="Home--header_text">
+        <h1 className="Home--header">Community Metalworks</h1>
+        <hr />
+        <h2 className="Home--header-sub">
+          Quality metal parts for world class businesses.
+        </h2>
+      </div>
     </div>
   );
 }
